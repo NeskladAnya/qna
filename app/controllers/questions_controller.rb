34b4b5185a2cc323c14660fea0a791d2cd4557c4
Hanna <belko.hanna.off@gmail.com>
2022-06-13 16:43:15 +0200

    Implement the answer new and create methods

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,4 @@
 class QuestionsController < ApplicationController
-  before_action :find_question, only: %i[show edit update destroy]
   def index
     @questions = Question.all
   end
@@ -8,7 +7,6 @@ class QuestionsController < ApplicationController
   end
 
   def new
-    @question = Question.new
   end
 
   def create
@@ -25,7 +23,7 @@ class QuestionsController < ApplicationController
   end
 
   def update
-    if @question.update(question_params)
+    if question.update(question_params)
       redirect_to @question
     else
       render :edit
@@ -33,14 +31,16 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question.destroy
+    question.destroy
     redirect_to questions_path
   end
 
   private
-  def find_question
-    @question = Question.find(params[:id])
+  def question
+    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
   end
+
+  helper_method :question
 
   def question_params
     params.require(:question).permit(:title, :body)
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :answer do
+    association :question
     body { "MyString" }
+
+    trait :invalid do
+      body { nil }
+    end
   end
 end
