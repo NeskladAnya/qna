9376eb73a7a370dc4d98fc296eeb604a9371e68d
Hanna <belko.hanna.off@gmail.com>
2022-06-17 10:39:10 +0200

    Implement deleting questions and answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,7 @@
 class AnswersController < ApplicationController
-  before_action :find_question
-
   def create
+    @question = Question.find(params[:question_id])
+
     @answer = @question.answers.new(answer_params)
     @answer.author = current_user
 
@@ -12,11 +12,14 @@ class AnswersController < ApplicationController
     end
   end
 
-  private
-  def find_question
-    @question = Question.find(params[:question_id])
+  def destroy
+    @answer = Answer.find(params[:id])
+    @answer.destroy
+
+    redirect_to @answer.question, notice: 'Answer deleted'
   end
 
+  private
   def answer_params
     params.require(:answer).permit(:body)
   end
---
+++ spec/features/answer/delete_answer_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature 'The author of the answer can delete it', %q{
+  Given I'm the author of the answer
+  And I'm signed in
+  Then I should be able to delete the answer
+} do
+
+  given(:user) { create(:user) }
+  given(:user2) { create(:user) }
+
+  given(:question) { create(:question, author: user) }
+  given!(:answer) { create(:answer, question: question, author: user) }
+
+  scenario 'An author deletes the answer' do
+    sign_in(user)
+    
+    visit question_path(question)
+    click_on 'Delete answer'
+
+    expect(page).to have_content 'Answer deleted'
+  end
+
+  scenario 'A user who is not the author tries to delete the question' do
+    sign_in(user2)
+    visit question_path(question)
+
+    expect(page).to_not have_content 'Delete answer'
+  end
+end
