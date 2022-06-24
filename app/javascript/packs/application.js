7627f84772ebdbccf8ea6c45429898ff54966e9a
Hanna <belko.hanna.off@gmail.com>
2022-06-24 14:15:22 +0200

    Implement question edit using Ajax

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "answers"
+import "questions"
 
 Rails.start()
 Turbolinks.start()
